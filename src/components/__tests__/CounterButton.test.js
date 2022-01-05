60db57f37e0083fd3b26d4809b53cbbbb99d1f6c
Nazar <wolfter12@gmail.com>
2022-01-05 23:13:30 +0200

    update CounterButton component tests

--- src/components/__tests__/CounterButton.test.js
+++ src/components/__tests__/CounterButton.test.js
@@ -3,11 +3,22 @@ import { shallow } from 'enzyme';
 
 import CounterButton from '../CounterButton';
 
-it('expect to render CounterButton component', () => {
-  const mockColor = 'red';
+describe('CounterButton component', () => {
+  it('renders CounterButton component', () => {
+    const mockColor = 'red';
 
-  const wrapper = shallow(<CounterButton color={mockColor} />);
+    const wrapper = shallow(<CounterButton color={mockColor} />);
 
-  expect(wrapper.length).toEqual(1);
-  expect(wrapper).toMatchSnapshot();
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('correctly increments the counter', () => {
+    const mockColor = 'red';
+
+    const wrapper = shallow(<CounterButton color={mockColor} />);
+
+    wrapper.find('[id="counter"]').simulate('click');
+    expect(wrapper.state()).toEqual({ count: 2 });
+  });
 });
