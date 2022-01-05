a5cd827a84ac03aa711969ecf68c0057f1ae796b
Nazar <wolfter12@gmail.com>
2022-01-05 22:48:38 +0200

    add tests for CounterButton component

---
+++ src/components/CounterButton.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CounterButton from './CounterButton';
+
+it('expect to render CounterButton component', () => {
+  const mockColor = 'red';
+
+  const wrapper = shallow(<CounterButton color={mockColor} />);
+
+  expect(wrapper.length).toEqual(1);
+  expect(wrapper).toMatchSnapshot();
+});
