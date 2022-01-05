db1860819eb80c55784d37f84e742f8236dac00e
Nazar <wolfter12@gmail.com>
2022-01-05 22:29:59 +0200

    add tests for Card component

---
+++ src/components/Card.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Card from './Card';
+
+it('expect to render Card component', () => {
+  const wrapper = shallow(<Card />);
+
+  expect(wrapper.length).toEqual(1);
+  expect(wrapper).toMatchSnapshot();
+});
