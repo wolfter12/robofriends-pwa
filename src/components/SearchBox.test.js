04f5023d052b775bd309ebfea3e41057d3e07409
Nazar <wolfter12@gmail.com>
2022-01-05 22:50:13 +0200

    add SearchBox component snapshot

---
+++ src/components/SearchBox.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SearchBox from './SearchBox';
+
+it('expect to render SearchBox component', () => {
+  const wrapper = shallow(<SearchBox />);
+
+  expect(wrapper.length).toEqual(1);
+  expect(wrapper).toMatchSnapshot();
+});
