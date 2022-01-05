9a864627e85845f5a58c4ca4408b07eca4591124
Nazar <wolfter12@gmail.com>
2022-01-06 00:11:38 +0200

    create MainPage component

---
+++ src/components/MainPage.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import ErrorBoundry from './ErrorBoundry';
+import Header from './Header';
+
+import './MainPage.css';
+
+class MainPage extends Component {
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+  filteredRobots = () => {
+    const { robots, searchField } = this.props;
+
+    return robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+  };
+
+  render() {
+    const { onSearchChange, isPending } = this.props;
+    return (
+      <div className='tc'>
+        <Header />
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          {isPending ? (
+            <h1>Loading</h1>
+          ) : (
+            <ErrorBoundry>
+              <CardList robots={this.filteredRobots()} />
+            </ErrorBoundry>
+          )}
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
